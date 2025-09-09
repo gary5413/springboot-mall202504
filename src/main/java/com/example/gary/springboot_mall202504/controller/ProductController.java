3a85c5740c3ac65826419f9636eeaf235f7828be
garylee <5413gary@gmail.com>
2025-09-09 16:05:42 +0800

    商品功能 查詢新增排序功能

--- src/main/java/com/example/gary/springboot_mall202504/controller/ProductController.java
+++ src/main/java/com/example/gary/springboot_mall202504/controller/ProductController.java
@@ -36,12 +36,18 @@ public class ProductController {
   
   @GetMapping("/products")
   public ResponseEntity<List<Product>> getProducts(
+//		  查詢條件 filtering
 		  @RequestParam(required = false) ProductCategory category,
-		  @RequestParam(required = false) String search
+		  @RequestParam(required = false) String search,
+//		  排序 sorting
+		  @RequestParam(defaultValue = "created_date") String orderBy,
+		  @RequestParam(defaultValue = "desc") String sort
 		  ){
 	  ProductQueryParams productQueryParams=new ProductQueryParams();
 	  productQueryParams.setCategory(category);
 	  productQueryParams.setSearch(search);
+	  productQueryParams.setOrderBy(orderBy);
+	  productQueryParams.setSort(sort);
 //	  改寫傳遞參數
 //	  List<Product> productList= productService.getProducts(category,search);
 	  List<Product> productList= productService.getProducts(productQueryParams);
